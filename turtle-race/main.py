7ee01a89a83e21684a32053a8ed76f6f6dc17c49
Alex Boyd <alexanderleeboyd@gmail.com>
2023-02-28 15:41:27 -0500

    Turtle creation loop

--- turtle-race/main.py
+++ turtle-race/main.py
@@ -5,8 +5,19 @@ screen.setup(width=500, height=400)
 
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 
-tim = Turtle()
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+turtle_dict = {}
+start_x = -230
+start_y = -100
 
+
+for color in colors:
+  turtle_dict[color] = Turtle(shape="turtle")
+  turtle = turtle_dict[color]
+  turtle.penup()
+  turtle.color(color)
+  turtle.goto(x=start_x,y=start_y)
+  start_y += 30
 
 
 screen.exitonclick()
