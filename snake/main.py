1ae27c345ca4bd5c555047d25e9fd03df9793443
Alex Boyd <alexanderleeboyd@gmail.com>
2023-03-01 14:09:08 -0500

    add food file

--- snake/main.py
+++ snake/main.py
@@ -1,5 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.tracer(0)
 screen.title("Snake Snax")
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
