8eb17e9e6e0a307ce1b4d259733ebd7d02a5fe07
Alex Boyd <alexanderleeboyd@gmail.com>
2023-03-06 15:38:18 -0500

    Create ball class

--- pong/main.py
+++ pong/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800,height=600)
@@ -9,15 +11,20 @@ screen.tracer(0)
 
 right_paddle = Paddle((350,0))
 left_paddle = Paddle((-350,0))
+ball = Ball()
 screen.update()
 
 screen.listen()
 screen.onkey(right_paddle.go_up, "Up")
 screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
   screen.update()
+  ball.move()
+  time.sleep(0.1)
 
 
 screen.exitonclick()
