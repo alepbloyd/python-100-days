5862cd4b4a23f843e3b5c77499c46ed1738b4368
Alex Boyd <alexanderleeboyd@gmail.com>
2023-03-07 12:54:50 -0500

    snake game high score functionality

--- snake/scoreboard.py
+++ snake/scoreboard.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 
-FONT = ('Arial', 40, 'normal')
+FONT = ('Arial', 32, 'normal')
 MOVE = False
 ALIGNMENT = "center"
 
@@ -9,19 +9,29 @@ class Scoreboard(Turtle):
   def __init__(self):
     super().__init__()
     self.current_score = 0
-    
+    self.high_score = 0
     self.speed("fastest")
     self.hideturtle()
     self.penup()
     self.color("white")
     self.goto(0,230)
     self.pendown()
-    self.write(self.current_score, move=MOVE, align=ALIGNMENT,font=FONT)
+    self.update_scoreboard()
     
   def increment_score(self):
     self.current_score += 1
+    self.update_scoreboard()
+
+  def update_scoreboard(self):
     self.clear()
-    self.write(self.current_score, move=MOVE, align=ALIGNMENT,font=FONT)
+    self.write(f"Score: {self.current_score} High Score: {self.high_score}", move=MOVE, align=ALIGNMENT,font=FONT)
+
+  def reset(self):
+    if self.current_score > self.high_score:
+      self.high_score = self.current_score
+    self.current_score = 0
+    self.update_scoreboard()
+
 
   def game_over(self):
     self.goto(0,0)
