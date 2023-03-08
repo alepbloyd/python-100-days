a63d9f86c0722ad2311698c914fd25fb0465bc68
Alex Boyd <alexanderleeboyd@gmail.com>
2023-03-08 14:08:48 -0500

    Weather and squirrel data

---
+++ squirrel-data/main.py
@@ -0,0 +1,20 @@
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+gray_squirrels = data[data["Primary Fur Color"] == "Gray"]
+cinnamon_squirrels = data[data["Primary Fur Color"] == "Cinnamon"]
+black_squirrels = data[data["Primary Fur Color"] == "Black"]
+
+gray_squirrels_count = len(gray_squirrels)
+cinnamon_squirrels_count = len(cinnamon_squirrels)
+black_squirrels_count = len(black_squirrels)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, cinnamon_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+
+df.to_csv("squirrel_count.csv")
